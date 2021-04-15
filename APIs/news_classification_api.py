c4c80c5117f21a192c92a1628b08cfafb05be4ef
isidoraconic <conic.i@northeastern.edu>
2021-04-15 00:01:03 -0700

    updated API code

--- APIs/news_classification_api.py
+++ APIs/news_classification_api.py
@@ -7,13 +7,17 @@ Navigate to:
 """
 import connexion
 from joblib import load
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2),
+                        stop_words='english')
 
 # Instantiate our Flask app object
 app = connexion.FlaskApp(__name__, port=8080, specification_dir='')
 application = app.app
 
 # Load our pre-trained model
-clf = load('./Classification_joblib')
+clf = load('./Classificatin.joblib')
 
 # Implement a simple health check function (GET)
 def health():
@@ -29,7 +33,12 @@ def health():
 def predict(article):
     # Accept the feature values provided as part of our POST
     # Use these as input to clf.predict()
-    prediction = clf.predict([[article]])
+
+    article = [article]
+    features = tfidf.fit_transform(article).toarray()
+    prediction = clf.predict(features)
+
+    print(prediction)
 
     # Map the predicted value to an actual class
 
